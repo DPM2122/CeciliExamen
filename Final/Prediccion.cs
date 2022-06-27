3fff7860b823ee273449800e9061679fcf40feb6
Desiree Moya <desmp7@gmail.com>
2022-06-27 17:53:32 +0200

    lol

--- Final/Prediccion.cs
+++ Final/Prediccion.cs
@@ -25,8 +25,9 @@ namespace Final
 
 
         /// <summary>
-        /// Constuctor que inicualiza las variables a 0
+        /// Constuctor que inicualiza las variables
         /// </summary>
+        
        public Prediccion ()
         {
             TempCelsius = 0;
@@ -35,10 +36,9 @@ namespace Final
             TempMin = 1000;
         }
 
-
-
         /// <summary>
         /// Funcion que predice la temperatura a raiz de tres dias de observacion
+        /// <para>calculamos la temperatura media total, dándo más peso al último día que al primero</para>
         /// </summary>
         /// <param name="observacionDia1">datos de orsevacion de dia 1</param>
         /// <param name="observacionDia2">datos de orsevacion de dia 2</param>
@@ -54,10 +54,6 @@ namespace Final
                    tempMedia2 = 0,
                    tempMedia3 = 0; // temperatura media de cada día
 
-           
-
-            // Para cada día obtenemos la suma de temperaturas
-            //
             if (observacionDia1.Count <= 0)
             {
                 return false;
@@ -65,6 +61,43 @@ namespace Final
                 // Tenemos que tener al menos una observación
             }
 
+            i = CalcularTemperaturaMediaDia(observacionDia1, ref tempMedia1);
+
+            if (observacionDia2.Count <= 0)
+            {
+                return false;
+                throw new Exception("Al menos tiene que existir una observación");
+                // Tenemos que tener al menos una observación
+            }
+
+            i = CalcularTemperaturaMediaDia(observacionDia2, ref tempMedia2);
+
+
+            if (observacionDia3.Count <= 0)
+            {
+                return false;
+                throw new Exception("Al menos tiene que existir una observación");
+                // Tenemos que tener al menos una observación
+            }
+
+            i = CalcularTemperaturaMediaDia(observacionDia3, ref tempMedia3);
+
+
+            // Finalmente calculamos la temperatura media total, dándo más peso 		
+            // al último día que al primero
+            //
+            TempCelsius = 0.2 * tempMedia1 + 0.35 * tempMedia2 + 0.45 * tempMedia3;
+
+            // calculamos también la temperatura en grados farenheit
+            //
+            TempFarenheit = (TempCelsius * 1.8) + 32;
+
+            return true;
+        }
+
+        private int CalcularTemperaturaMediaDia(List<double> observacionDia1, ref double tempMedia1)
+        {
+            int i;
             for (i = 0; i < observacionDia1.Count; i++)
             {
                 tempMedia1 = tempMedia1 + observacionDia1[i];
@@ -77,72 +110,11 @@ namespace Final
                 if (TempMax > observacionDia1[i])
                 {
                     TempMax = observacionDia1[i];
-
                 }
             }
 
             tempMedia1 = tempMedia1 / observacionDia1.Count;
-
-            if (observacionDia2.Count <= 0)
-            {
-                return false;
-                throw new Exception("Al menos tiene que existir una observación");
-                // Tenemos que tener al menos una observación
-            }
-
-            for (i = 0; i < observacionDia2.Count; i++)
-            {
-                tempMedia2 = tempMedia2 + observacionDia2[i];
-
-                if (TempMin < observacionDia2[i])
-                {
-                    TempMin = observacionDia2[i];
-                }
-
-                if (TempMax > observacionDia2[i])
-                {
-                    TempMax = observacionDia2[i];
-
-                }
-            }
-
-            tempMedia2 = tempMedia2 / observacionDia2.Count;
-
-            if (observacionDia3.Count <= 0)
-            {
-                return false;
-                throw new Exception("Al menos tiene que existir una observación");
-                // Tenemos que tener al menos una observación
-            }
-
-            for (i = 0; i < observacionDia3.Count; i++)
-            {
-                tempMedia3 = tempMedia3 + observacionDia3[i];
-
-                if (TempMin < observacionDia3[i])
-                {
-                    TempMin = observacionDia1[i];
-                }
-
-                if (TempMax > observacionDia3[i])
-                {
-                    TempMax = observacionDia1[i];
-
-                }
-            }
-
-            tempMedia3 = tempMedia3 / observacionDia3.Count;
-
-            // Finalmente calculamos la temperatura media total, dándo más peso 		
-            // al último día que al primero
-            //
-            TempCelsius = 0.2 * tempMedia1 + 0.35 * tempMedia2 + 0.45 * tempMedia3;
-
-            // calculamos también la temperatura en grados farenheit
-            //
-            TempFarenheit = (TempCelsius * 1.8) + 32;
-
-            return true;
+            return i;
         }
     }
 }
--- PruebaFinal/PruebaPrediccion.cs
+++ PruebaFinal/PruebaPrediccion.cs
@@ -10,41 +10,48 @@ namespace PruebaFinal
     public class PruebaPrediccion
     {
         [TestMethod]
-        public void PruebaTemperaturaFueraRango() //Esta prueba pretende sacar un error si la temperatura máxima o mínima es mayor o menor de lo introducido en la clase
+        public void PruebaDatosCorrectos() //Esta prueba pretende sacar un error si la temperatura máxima o mínima es mayor o menor de lo introducido en la clase
         {
             Prediccion miPrediccionPrueba = new Prediccion();
-
             List<double> dia1 = new List<double>(),
                          dia2 = new List<double>(),
                          dia3 = new List<double>();
 
-            dia1.Add(12.5); dia1.Add(16.5); dia1.Add(21); dia1.Add(17); dia1.Add(15);
-            dia2.Add(13); dia2.Add(15); dia2.Add(19.5); dia2.Add(16.5); dia2.Add(14);
-            dia3.Add(14.5); dia3.Add(18.5); dia3.Add(23); dia3.Add(20); dia3.Add(17.5);
+            dia1.Add(12.5);
+            dia1.Add(16.5);
+            dia1.Add(21);
+            dia1.Add(17);
+            dia1.Add(15);
+            dia2.Add(13);
+            dia2.Add(15);
+            dia2.Add(19.5);
+            dia2.Add(16.5);
+            dia2.Add(14);
+            dia3.Add(14.5); 
+            dia3.Add(18.5);
+            dia3.Add(23); 
+            dia3.Add(20);
+            dia3.Add(17.5);
 
-            miPrediccionPrueba.TempMax = 1001;
+            double temperaturaMediaDia1 = 16.4;
+            double temperaturaMediaDia2 = 15.6;
+            double temperaturaMediaDia3 = 18.7;
+            double temperaturaMaxEsperada = 23;
+            double temperaturaMinEsperada = 12.5;
+            double celsuiusEsperados = 17.155;
+            double farenheitEsperados = 62.879;
+
+            Assert.AreEqual(celsuiusEsperados, miPrediccionPrueba.TempCelsius, "Los valores no son correctos");
+            Assert.AreEqual(farenheitEsperados, miPrediccionPrueba.TempFarenheit, "Los valores no son correctos");
+            Assert.AreEqual(temperaturaMaxEsperada, miPrediccionPrueba.TempMax, "Los valores no son correctos");
+            Assert.AreEqual(temperaturaMinEsperada, miPrediccionPrueba.TempMin, "Los valores no son correctos");
 
 
-            if (miPrediccionPrueba.TempMax <= 1000)
-            {
-                miPrediccionPrueba.PrediccionTemperatura(dia1, dia2, dia3);
 
 
-                Console.WriteLine("Temperatura Celsius: " + miPrediccionPrueba.TempCelsius);
-                Console.WriteLine("Temperatura Farenheit: " + miPrediccionPrueba.TempFarenheit);
-                Console.WriteLine("Máxima: " + miPrediccionPrueba.TempMax);
-                Console.WriteLine("Mínima: " + miPrediccionPrueba.TempMin);
-
-                Console.ReadKey();
 
 
-            }
-            else
-            {
 
-                throw new ArgumentOutOfRangeException("La temperatura máxima es incorrecta");
-
-            }
 
 
         }
